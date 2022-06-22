cfe81f865b49783aa2d2d0f5188895841b18c8e9
viniciusdds <viniciusdamasceno180@hotmail.com>
2022-06-22 14:52:14 -0300

    move EmailInput to its own file

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../components/components.dart';
+import 'components/components.dart';
 import 'login_presenter.dart';
 
 class LoginPage extends StatefulWidget {
@@ -49,58 +51,48 @@ class _LoginPageState extends State<LoginPage> {
                 Headline1(text: 'Login'),
                 Padding(
                   padding: const EdgeInsets.all(32),
-                  child: Form(
-                    child: Column(
-                      children: [
-                        StreamBuilder<dynamic>(
-                            stream: widget.presenter.emailErrorStream,
-                            builder: (context, snapshot) {
-                              return TextFormField(
-                                decoration: InputDecoration(
-                                    labelText: "Email",
-                                    icon: Icon(Icons.email, color: Theme.of(context).primaryColorLight),
-                                    errorText: snapshot.data == '' ? null : snapshot.data
-                                ),
-                                keyboardType: TextInputType.emailAddress,
-                                onChanged: widget.presenter.validateEmail,
-                              );
-                            }
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 8, bottom: 32),
-                          child: StreamBuilder<dynamic>(
-                              stream: widget.presenter.passwordErrorStream,
+                  child: Provider(
+                    create: (_) => widget.presenter,
+                    child: Form(
+                      child: Column(
+                        children: [
+                          EmailInput(),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 8, bottom: 32),
+                            child: StreamBuilder<dynamic>(
+                                stream: widget.presenter.passwordErrorStream,
+                                builder: (context, snapshot) {
+                                  return TextFormField(
+                                    decoration: InputDecoration(
+                                        labelText: "Senha",
+                                        icon: Icon(Icons.lock, color: Theme.of(context).primaryColorLight),
+                                        errorText: snapshot.data == '' ? null : snapshot.data
+                                    ),
+                                    obscureText: true,
+                                    onChanged: widget.presenter.validatePassword,
+                                  );
+                                }
+                            ),
+                          ),
+                          StreamBuilder<dynamic>(
+                              stream: widget.presenter.isFormValidStream,
                               builder: (context, snapshot) {
-                                return TextFormField(
-                                  decoration: InputDecoration(
-                                      labelText: "Senha",
-                                      icon: Icon(Icons.lock, color: Theme.of(context).primaryColorLight),
-                                      errorText: snapshot.data == '' ? null : snapshot.data
-                                  ),
-                                  obscureText: true,
-                                  onChanged: widget.presenter.validatePassword,
+                                return ElevatedButton(
+                                    onPressed: snapshot.data == true ? widget.presenter.auth : null,
+                                    style: ElevatedButton.styleFrom(
+                                        primary: Theme.of(context).primaryColor
+                                    ),
+                                    child: Text('Entrar'.toUpperCase())
                                 );
                               }
                           ),
-                        ),
-                        StreamBuilder<dynamic>(
-                            stream: widget.presenter.isFormValidStream,
-                            builder: (context, snapshot) {
-                              return ElevatedButton(
-                                  onPressed: snapshot.data == true ? widget.presenter.auth : null,
-                                  style: ElevatedButton.styleFrom(
-                                      primary: Theme.of(context).primaryColor
-                                  ),
-                                  child: Text('Entrar'.toUpperCase())
-                              );
-                            }
-                        ),
-                        TextButton.icon(
-                            onPressed: (){},
-                            icon: Icon(Icons.person),
-                            label: Text('Criar Conta')
-                        )
-                      ],
+                          TextButton.icon(
+                              onPressed: (){},
+                              icon: Icon(Icons.person),
+                              label: Text('Criar Conta')
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 )
@@ -112,7 +104,6 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 }
-
 
 
 
