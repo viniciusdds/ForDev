7225245d27366cd551bd839bb3b72d32f888896b
viniciusdds <viniciusdamasceno180@hotmail.com>
2022-07-17 14:54:03 -0300

    ensure SteamLoginPresenter emits correct events on Authentication

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -4,6 +4,7 @@ import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'package:fordev/domain/entities/entities.dart';
 import 'package:fordev/domain/usecases/usecases.dart';
 
 import 'package:fordev/presentation/presenters/presenters.dart';
@@ -26,6 +27,12 @@ void main(){
     mockValidationCall(field).thenReturn(value);
   }
 
+  PostExpectation mockAuthenticationCall() => when(authentication.auth(any));
+
+  void mockAuthentication(){
+    mockAuthenticationCall().thenAnswer((_) async => AccountEntity(faker.guid.guid()));
+  }
+
   setUp((){
        validation = ValidationSpy();
        authentication = AuthenticationSpy();
@@ -33,6 +40,7 @@ void main(){
        email = faker.internet.email();
        password = faker.internet.password();
        mockValidation();
+       mockAuthentication();
   });
 
   test('Should call Validation with correct email', (){
@@ -113,5 +121,14 @@ void main(){
     await sut.auth();
 
     verify(authentication.auth(AuthenticationParams(email: email, secret: password))).called(1);
+  });
+
+  test('Should emit correct events on Authentication success', () async {
+    sut.validateEmail(email);
+    sut.validatePassword(password);
+
+    expectLater(sut.isLoadingStream, emitsInOrder([true, false]));
+
+    await sut.auth();
   });
 }
